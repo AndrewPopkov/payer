2715fa88bde49ab0d8aa2a4c871f3ed04376fc8e
Андрей <Andypriestman@yandex.ru>
2017-04-03 20:30:02 +0300

    1

---
+++ serverpayer/serverpayer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using serverHttp;
+using System.Net;
+
+namespace serverpayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем
+            Server server = new Server();
+            // Запускаем
+            server.start(IPAddress.Parse("127.0.0.1"), 80, 10, contentpath);
+            // останавливаем
+            //server.stop();
+        }
+    }
+}
